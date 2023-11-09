a8534ca7c5eeb4392ff8c0650c818671d917ec6a
lynn baydoun <lkb07@mail.aub.edu>
2023-11-09 11:20:58 +0200

    starting

---
+++ server/src/models/review.model.js
@@ -0,0 +1,33 @@
+import mongoose, { Schema } from 'mongoose';
+import modelOptions from './model.options.js';
+export default mongoose.model(
+    "Review",
+    mongoose.Schema({
+        user: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+            required: true
+        },
+        content: {
+            type: string,
+            required: true
+        },
+        mediaType: {
+            type: string,
+            enum: ["tv", "movie"],
+            required: true
+        },
+        mediaId: {
+            type: string,
+            required: true
+        },
+        mediaTitle: {
+            type: string,
+            required: true
+        },
+        mediaPoster: {
+            type: string,
+            required: true
+        },
+    }, modelOptions)
+)
