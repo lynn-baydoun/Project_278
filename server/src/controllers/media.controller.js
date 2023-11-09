278bc0478565b7690681edf6cbfa99221edeb4cb
lynn-baydoun <lkb07@mail.aub.edu>
2023-11-09 16:11:14 +0200

    media crtl

--- server/src/controllers/media.controller.js
+++ server/src/controllers/media.controller.js
@@ -4,6 +4,7 @@ import userModel from "../models/user.model.js";
 import favoriteModel from "../models/favorite.model.js";
 import reviewModel from "../models/review.model.js";
 import tokenMiddleware from "../middlewares/token.middleware.js"
+
 const getList = async (req, res) =>{
     try {
         const {page} = req.body;
@@ -46,6 +47,7 @@ const getDetail = async(req, res)=>{
     try {
         const {mediaType, mediaId} = req.params;
         const params = {mediaType, mediaId};
+        
         const media = await tmdbApi.mediaDetail(params);
         media.credits = await tmdbApi.mediaCredits(params);
         const videos = await tmdbApi.mediaVideos(params)
