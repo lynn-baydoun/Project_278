8c4a9cd2f0e3e7ffd77af67a02633b88c80610a0
lynn-baydoun <lkb07@mail.aub.edu>
2023-11-17 13:36:00 +0200

    feat : api-service-module-person

---
+++ client/src/api/modules/person.api.js
@@ -0,0 +1,23 @@
+import privateClient from "../client/private.client";
+
+const personEndpoints = {
+    detail : personId => `person/${personId}`,
+    medias : personId => `person/${personId}/medias`,
+}
+
+const personApi = {
+    detail : async({personId}) =>{
+        try{
+            const response = await privateClient.get(personEndpoints.detail(personId));
+            return {response};
+        }catch(err) {return {err}};
+    },
+    medias : async({personId}) =>{
+        try{
+            const response = await privateClient.get(personEndpoints.medias(personId));
+            return {response};
+        }catch(err) {return {err}};
+    }
+}
+
+export default personApi;
