9f08c1a3a93f664b039ee5ad55536e0160aedd13
lynn-baydoun <aaz29@mail.aub.edu>
2023-11-20 15:57:48 +0200

    feat : theme-Mode-utils

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,22 @@
+import { ThemeProvider } from "@mui/material/styles";
+import { useSelector } from "react-redux";
+import themeConfigs from "./configs/theme.configs";
 const App = () => {
+  const {themeMode} = useSelector((state) => state.themeMode);
   return (
-    <div className="App">
-      App
-    </div>
+    <ThemeProvider theme = {themeConfigs.custom({mode : themeMode})}> 
+    {/* config toastify*/}
+    <ToastContainer>
+      position = "bottom-left";
+      autoClose = {5000}
+      hideProgressBar = {false}
+      newestOnTop = {false}
+      closeOnClick
+      pauseOnFocusLoss
+      pauseOnHover
+      theme={themeMode}
+    </ToastContainer>
+    </ThemeProvider>
   );
 }
 
---
+++ client/src/configs/theme.configs.js
@@ -0,0 +1,56 @@
+import { createTheme} from "@mui/material/styles"; 
+import {colors} from "@mui/material";
+
+export const themeModes = {
+    dark : "dark",
+    light : "light"
+}
+
+getCustomPallete = (mode) =>{
+    if(mode === themeModes.dark) {
+        return customPallete = {
+            primary : {
+                main :"#ff0000",
+                contrastText: "#ffffff"
+            },
+            secondary :{
+                main :"#f44336",
+                contrastText: "#ffffff"
+            },
+            background : {
+                default : "#000000",
+                paper: "#131313",
+            }
+        }
+    }
+    return customPallete = {
+        primary : {
+            main :"#ff0000",
+        },
+        secondary :{
+            main :"#f44336",
+        },
+        background : {
+            default : colors.grey["100"]
+        }
+    }
+}
+
+const themeConfigs = {
+    custom : (mode) =>{
+
+        return createTheme({
+            palette : {
+                mode, 
+                ...getCustomPallete(mode)
+            },
+            components : {
+                MuiButton : {
+                    defaultProps : {disableElevation : true}
+                }
+            }
+        })
+    }
+}
+
+export default themeConfigs;
